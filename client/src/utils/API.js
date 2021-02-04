b0f3c6a8ea10c9e8b231b47a88b14abfd6620aa0
cpusillo <cpusillo@gmail.com>
2021-02-04 18:21:32 -0500

    Setup api calls via axios

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,13 +8,13 @@ export default {
         getBook(title){
             return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=${api}`);
          },
-        saveBook(){
-                
+        saveBook(bookData){
+                return axios.post("/api/books", bookData);
         },
         getSavedBooks(){
-
+                return axios.get("/api/books");
         },
         deleteBook(){
-
+                return axios.delete("/api/books/" + id);
         }
 }
