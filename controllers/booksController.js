9bbd76be048a88c28f90e8ea169d3676db0b976f
Corrine <cpusillo@gmail.com>
2021-02-05 16:41:38 -0500

    Fixing api/books issues

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,39 +1,37 @@
 const db = require("../models");
 
+// Defining methods for the booksController
 module.exports = {
-    // Find all the books that are saved to our db.
   findAll: function(req, res) {
-    db.Book.find(req.query).then(data => res.json(data))
-      .catch(err => {
-        console.error(err)
-      });
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
-    // Find saved book by its unique ID in our db.
   findById: function(req, res) {
-    db.Book.findById(req.params.id).then(data => res.json(data))
-      .catch(err => {
-        console.error(err)
-      });
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
-    // Create a new book
   create: function(req, res) {
-    db.Book.create(req.body).then(data => res.json(data))
-      .catch(err => {
-        console.error(err)
-      });
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
-    // Update a book record
   update: function(req, res) {
-    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body).then(data=> res.json(data))
-      .catch(err => {
-        console.error(err)
-      });
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
-    // Delete a book record
   remove: function(req, res) {
-    db.Book.findById({ _id: req.params.id }).then(data => data.remove()).then(data => res.json(data))
-      .catch(err => {
-        console.error(err)
-      });
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 };
