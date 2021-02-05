572c1ca506005bebb65b4d4d64d25522faf9c22d
cpusillo <cpusillo@gmail.com>
2021-02-05 17:00:56 -0500

    Fixed navbar with router

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Navbar, Nav} from 'react-bootstrap'
 import { FaBook } from 'react-icons/fa'
 
 export default function Navigation() {
@@ -7,11 +8,34 @@ export default function Navigation() {
         <div>
             <Navbar bg="light" expand="lg">
             <Navbar.Brand className="brand" href="#home">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
             <FaBook className="blue" /> 
             <span className="red"> Google </span> 
             <span className="yellow">Book </span>
             <span className="green">Search </span>
+            </Link>
             </Navbar.Brand>
+            <Nav className="ml-auto">
+            <Nav.Link>
+            <Link
+              to="/saved"
+              className={
+                window.location.pathname === "/Saved" || window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Saved Books
+            </Link>
+            </Nav.Link>
+            </Nav>
         </Navbar>
         </div>
     )
